bddb1bbe3a103186e35f659ccd075f098474fcd4
yeswanth-maddi <127112343+yeswanth-maddi@users.noreply.github.com>
2023-12-31 12:12:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+const express=require("express")
+const mongoose=require("mongoose");
+const bodyPareser=require("body-parser");
+const dotenv=require("dotenv");
+const bodyParser = require("body-parser");
+
+const app=express();
+dotenv.config();
+
+const port=process.env.PORT || 3500;
+
+const username=process.env.MONGODB_USERNAME;
+const password=process.env.MONGODB_PASSWORD;
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.o3yx37g.mongodb.net/registrationFromDB`,{
+});
+
+const registrationSchema= new mongoose.Schema({
+    firstname : String,
+    lastname : String,
+    email : String,
+    message : String,
+});
+
+const Registration= mongoose.model("Registration",registrationSchema);
+
+app.use(bodyParser.urlencoded ({extended:true}));
+app.use(bodyParser.json());
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/pages/index.html");
+})
+
+app.post("/register",async(req,res)=>{
+    try{
+        const {firstname,lastname,email,message}=req.body;
+        const existingUser = await Registration.findOne({email : email});
+        if(!existingUser){
+            const registrationData = new Registration({
+                firstname,
+                lastname,
+                email,
+                message,
+            });
+            await registrationData.save();
+            res.redirect("/success");
+        }
+        else{
+            console.log("User already exist");
+            res.redirect("/error");
+        }
+    }
+    catch (error){
+        console.log(error);
+        res.redirect("/error");
+    }
+})
+
+app.get("/success",(req,res)=>{
+    res.sendFile(__dirname+"/pages/success.html");
+})
+
+app.get("/error",(req,res)=>{
+    res.sendFile(__dirname+"/pages/error.html");
+})
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
